beb1e8c6e2d5bca9dd55eb0d17981f4edfafb3e8
yordanos-bogale5 <bogaleyordanos64@gmail.com>
2024-11-11 14:09:50 +0000

    animations

--- lib/welcome_screen/welcome_screen.dart
+++ lib/welcome_screen/welcome_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '_welcome_bloc.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
   static Widget builder(BuildContext context) {
@@ -14,6 +14,42 @@ class WelcomeScreen extends StatelessWidget {
       )..add(WelcomeInitialEvent()),
       child: const WelcomeScreen(),
     );
+  }
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  double _opacity1 = 0.0;
+  double _opacity2 = 0.0;
+  double _opacity3 = 0.0;
+  double _opacity4 = 0.0;
+  double _opacity5 = 0.0;
+  double _opacity6 = 0.0;
+  double _opacity7 = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _animateContent();
+  }
+
+  Future<void> _animateContent() async {
+    await Future.delayed(const Duration(milliseconds: 500));
+    setState(() => _opacity1 = 1.0);
+    await Future.delayed(const Duration(milliseconds: 500));
+    setState(() => _opacity2 = 1.0);
+    await Future.delayed(const Duration(milliseconds: 500));
+    setState(() => _opacity3 = 1.0);
+    await Future.delayed(const Duration(milliseconds: 500));
+    setState(() => _opacity4 = 1.0);
+    await Future.delayed(const Duration(milliseconds: 500));
+    setState(() => _opacity5 = 1.0);
+    await Future.delayed(const Duration(milliseconds: 500));
+    setState(() => _opacity6 = 1.0);
+    await Future.delayed(const Duration(milliseconds: 500));
+    setState(() => _opacity7 = 1.0);
   }
 
   @override
@@ -51,94 +87,145 @@ class WelcomeScreen extends StatelessWidget {
                   padding: const EdgeInsets.only(bottom: 250.0),
                   child: Positioned(
                     bottom: 0,
-                    child: Image.asset(
-                      "assets/images/image 51.png",
-                      height: 540,
-                      width: MediaQuery.of(context).size.width,
-                      fit: BoxFit.cover,
+                    child: AnimatedOpacity(
+                      opacity: _opacity1,
+                      duration: const Duration(milliseconds: 500),
+                      child: Image.asset(
+                        "assets/images/image 51.png",
+                        height: 540,
+                        width: MediaQuery.of(context).size.width,
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
                 ),
                 Column(
                   mainAxisSize: MainAxisSize.max,
                   children: [
-                    const Padding(
-                      padding: EdgeInsets.only(top: 65.0),
-                      child: Text(
-                        "MERRY CHRISTMAS TO",
+                    AnimatedOpacity(
+                      opacity: _opacity2,
+                      duration: const Duration(milliseconds: 500),
+                      child: const Padding(
+                        padding: EdgeInsets.only(top: 65.0),
+                        child: Text(
+                          "MERRY CHRISTMAS TO",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Color(0xFFBD0D2C),
+                            fontSize: 31,
+                            fontFamily: 'Rye',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ),
+                    AnimatedOpacity(
+                      opacity: _opacity3,
+                      duration: const Duration(milliseconds: 500),
+                      child: const Text(
+                        "NAME",
                         textAlign: TextAlign.center,
                         style: TextStyle(
-                          color: Color(0xFFBD0D2C),
-                          fontSize: 31,
-                          fontFamily: 'Rye',
+                          color: Color(0xFFFFFFFF),
+                          fontSize: 45,
+                          fontFamily: 'Rosarivo',
                           fontWeight: FontWeight.w400,
                         ),
                       ),
                     ),
-                    const Text(
-                      "NAME",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Color(0xFFFFFFFF),
-                        fontSize: 45,
-                        fontFamily: 'Rosarivo',
-                        fontWeight: FontWeight.w400,
+                    AnimatedOpacity(
+                      opacity: _opacity4,
+                      duration: const Duration(milliseconds: 500),
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 15.0),
+                        child: Image.asset(
+                          "assets/images/Variant2.png",
+                          height: 24,
+                          width: 350,
+                          fit: BoxFit.contain,
+                        ),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 15.0),
+                    // Overlapping GIFs
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        AnimatedOpacity(
+                          opacity: _opacity5,
+                          duration: const Duration(milliseconds: 500),
+                          child: Image.asset(
+                            "assets/images/Animation - 1731329179405.gif",
+                            height: 140,
+                            width: 229,
+                          ),
+                        ),
+                        AnimatedOpacity(
+                          opacity: _opacity5,
+                          duration: const Duration(milliseconds: 500),
+                          child: Image.asset(
+                            "assets/images/stripes.gif",
+                            height: 140,
+                            width: 229,
+                          ),
+                        ),
+                      ],
+                    ),
+                    AnimatedOpacity(
+                      opacity: _opacity6,
+                      duration: const Duration(milliseconds: 500),
                       child: Image.asset(
-                        "assets/images/Variant2.png",
-                        height: 24,
-                        width: 350,
-                        fit: BoxFit.contain,
+                        "assets/images/Group 7517 (1).png",
+                        height: 175,
+                        width: 381,
                       ),
-                    ),
-                    Image.asset(
-                      "assets/images/christmas WELCOME PAGE.png",
-                      height: 140,
-                      width: 229,
-                    ),
-                    Image.asset(
-                      "assets/images/Group 7517 (1).png",
-                      height: 175,
-                      width: 381,
                     ),
                     const SizedBox(height: 40),
-                    const Text(
-                      "JUST TYPE IN",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Color(0xFFFFFFFF),
-                        fontSize: 24,
-                        fontFamily: 'Ribeye',
-                        fontWeight: FontWeight.w400,
+                    AnimatedOpacity(
+                      opacity: _opacity7,
+                      duration: const Duration(milliseconds: 500),
+                      child: const Column(
+                        children: [
+                          Text(
+                            "JUST TYPE IN",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color(0xFFFFFFFF),
+                              fontSize: 24,
+                              fontFamily: 'Ribeye',
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          Text(
+                            "ALL YOUR WISHES",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color(0xFFFFFFFF),
+                              fontSize: 24,
+                              fontFamily: 'Ribeye',
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          Text(
+                            "FOR CHRISTMAS!",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color(0xFFFFFFFF),
+                              fontSize: 31,
+                              fontFamily: 'Ribeye',
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    const Text(
-                      "ALL YOUR WISHES",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Color(0xFFFFFFFF),
-                        fontSize: 24,
-                        fontFamily: 'Ribeye',
-                        fontWeight: FontWeight.w400,
+                    AnimatedOpacity(
+                      opacity: _opacity7,
+                      duration: const Duration(milliseconds: 500),
+                      child: Image.asset(
+                        "assets/images/Group 8699.png",
+                        height: 110,
+                        width: 381,
                       ),
-                    ),
-                      const Text(
-                      "FOR CHRISTMAS!",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Color(0xFFFFFFFF),
-                        fontSize: 31,
-                        fontFamily: 'Ribeye',
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                    Image.asset(
-                      "assets/images/Group 8699.png",
-                      height: 110,
-                      width: 381,
                     ),
                   ],
                 ),
